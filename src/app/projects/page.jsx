61745e377f351ad85169c9baf57e998fda6859ca
Pepeoawvh <jpp.valdes@gmail.com>
2024-03-26 11:42:02 -0300

    [fix]-margen projectsection

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectsSection from "../components/ProjectsSection";
 const ProjectsPage = () => {
   return (
-    <main className="sm:mt-0 mt-36">
+    <main className="mt-36 transition-opacity from-0% to-100%">
       <div className="mx-12 mb-10  ">
         <ProjectsSection />
       </div>
