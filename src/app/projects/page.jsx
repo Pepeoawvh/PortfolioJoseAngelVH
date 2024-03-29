ab5df4b118dcbcc531568d89f9bb9e0bf5bb9f67
Pepeoawvh <jpp.valdes@gmail.com>
2024-03-29 08:23:50 -0300

    [fix]importacion archivo eliminado

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectsSection from "../components/ProjectsSection";
 const ProjectsPage = () => {
   return (
-    <main className={`mt-36 transition-opacity from-0% to-100% ${styles.fadeInCustom}`}>
+    <main className={`mt-36 animate-fade animate-delay-300`}>
       <div className="mx-12 mb-10  ">
         <ProjectsSection />
       </div>
