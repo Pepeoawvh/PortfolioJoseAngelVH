a78e7a75631070b85ae2fd7b9062d2e9a7b28df4
Pepeoawvh <jpp.valdes@gmail.com>
2024-05-01 20:47:22 -0400

    fix contacto margintop

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -5,7 +5,7 @@ import EmailSection from "../components/EmailSection";
 const ContactPage = () => {
   return (
     <main className="">
-      <div className="sm:mx-64 mx-12 mt-24">
+      <div className="sm:mx-64 mx-12">
         <EmailSection />
         {/* <NavBar2 /> */}
       </div>
